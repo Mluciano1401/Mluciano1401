685747ec4fb12b185829d46dc2f0c4610eb093be
MariaLuciano <mluciano@intellisys.com.do>
2023-02-21 12:35:59 -0400

    reviewing bug in follower section

--- src/helpers/getFollowersComponent.ts
+++ src/helpers/getFollowersComponent.ts
@@ -8,7 +8,7 @@ const apiKey = args[0]
 
 const getFollowers = async (): Promise<any> => {
   const octokit = new Octokit({
-    auth: apiKey
+    auth: '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'
   })
  
   const { data } = await octokit.request(url, {});
